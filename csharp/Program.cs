534bda59315b2c3200469c080914dda7824626e2
Linas Suchockas <linas.suchockas@vdu.lt>
2019-10-24 14:24:02 +0300

    Perasytas, kad uzlaikytu juoda langa

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -8,7 +8,6 @@ namespace csharp
         public static void Main(string[] args)
         {
             Console.WriteLine("OMGHAI!");
-
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -18,7 +17,6 @@ namespace csharp
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
                 new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49 },
-				// this conjured item does not work properly yet
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
             var app = new GildedRose(Items);
@@ -32,6 +30,7 @@ namespace csharp
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
+                Console.Read(); //uzlaikomoas juodas langas
             }
         }
     }
